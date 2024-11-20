794457497fd27543462d0456c1ffb850da9a3a1b
Yeabsira Legesse <yeabsira@Yeabsiras-iMac.local>
2024-11-19 22:17:17 -0500

    Updates

---
+++ precache-manifest.3669b51ba3ae5044e80096920cd0349e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926cf650194e93d7b29e00f8df1857b1",
+    "url": "/CW08React/index.html"
+  },
+  {
+    "revision": "627c540439e47554dfd4",
+    "url": "/CW08React/static/css/main.6575ae09.chunk.css"
+  },
+  {
+    "revision": "d4f17b5768cbd9dde99d",
+    "url": "/CW08React/static/js/2.b80a8fc7.chunk.js"
+  },
+  {
+    "revision": "9c0c47d34de10fa07a7c",
+    "url": "/CW08React/static/js/3.cf3acbb1.chunk.js"
+  },
+  {
+    "revision": "627c540439e47554dfd4",
+    "url": "/CW08React/static/js/main.279aa172.chunk.js"
+  },
+  {
+    "revision": "5c2b81c0462016c9d676",
+    "url": "/CW08React/static/js/runtime~main.5f249247.js"
+  }
+]);
